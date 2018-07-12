b57996fd6fbb6e46d9161f8fa1308b2117a1d640
soong <sts626@naver.com>
2018-07-12 17:52:03 +0900

    index.php 수정하였음. ~

--- yagaja_project/index.php
+++ yagaja_project/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>야! 가자~</title>
+  <title>야! ~가자~</title>
    <link rel="stylesheet" href="./common_css/index_css3.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
