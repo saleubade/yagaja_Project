18d71d016c188e4f920dd222d19f600888f3b3e6
ookkll2 <ookkll2@naver.com>
2018-10-10 22:22:29 +0900

    ㅁㄴㅇㄴㅁㅇㅁㄴㅇ

--- yagaja_project/index.php
+++ yagaja_project/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>?? ~ê°1€??</title>
+  <title>?? ~ê°1€232??</title>
    <link rel="stylesheet" href="./common_css/index_css3.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
